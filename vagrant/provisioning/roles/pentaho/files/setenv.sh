5554facb0802b1cfcbc262dab8befd7d791b6e68
David Miller <david.miller@armedia.com>
2019-01-03 15:50:35 -0500

    AFDP-7175 Pentaho CE

---
+++ vagrant/provisioning/roles/pentaho/files/setenv.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+CATALINA_OPTS="-Xms1024m -Xmx2048m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=50504 -Dcom.sun.management.jmxremote.rmi.port=50505 -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=arkcase-ce.local -Dcom.sun.management.jmxremote.password.file=/opt/app/pentaho/password.file -Dcom.sun.management.jmxremote.access.file=/opt/app/pentaho/access.file -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
+export CATALINA_OPTS
+export CATALINA_OUT=/opt/log/pentaho/catalina.out
+export CATALINA_TMPDIR=/opt/tmp/pentaho
+export JAVA_OPTS="-Djava.net.preferIPv4Stack=true"
