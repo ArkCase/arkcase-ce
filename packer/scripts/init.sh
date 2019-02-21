34859f627ab5e8b6fd29ef883ec154a90ea402ec
David Miller <david.miller@armedia.com>
2019-02-21 15:02:00 -0500

    Update master branch for arkcase-ce 1.0 release

---
+++ packer/scripts/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eux
+
+# Add vagrant user to sudoers.
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+
