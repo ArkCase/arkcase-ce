43c8fca2ece3b4f86147b64d59a6dcaca7adb9f2
Kristijan Manaskov <kristijan.manaskov@armedia.com>
2020-04-07 13:27:34 +0200

    add new role for starting config server

--- vagrant/scripts/set-hyperv-switch.ps1
+++
@@ -1,3 +0,0 @@
-# See: https://www.thomasmaurer.ch/2016/01/change-hyper-v-vm-switch-of-virtual-machines-using-powershell/
-
-Get-VM "arkcase-vagrant" | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName "NATSwitch"
