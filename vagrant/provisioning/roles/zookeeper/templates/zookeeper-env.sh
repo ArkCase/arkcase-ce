9cb2beca8cc99bb4174ee384767d5da9b0cdabfc
Cloud User <centos@eeocftsprodarkcase.ecmontap.com>
2020-06-26 16:10:31 +0000

    Latest drop of AWS enhancements

--- vagrant/provisioning/roles/zookeeper/templates/zookeeper-env.sh
+++ vagrant/provisioning/roles/zookeeper/templates/zookeeper-env.sh
@@ -1,4 +1,4 @@
 ZOO_LOG_DIR="{{ root_folder }}/log/zookeeper"
 ZOO_LOG4J_PROP="INFO,ROLLINGFILE"
 
-SERVER_JVMFLAGS="-Xms2048m -Xmx2048m -verbose:gc -Xlog:gc* -Xlog:gc:$ZOO_LOG_DIR/zookeeper_gc.log"
+SERVER_JVM_FLAGS="-Xms2048m -Xmx2048m"
